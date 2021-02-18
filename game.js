4a29af4e0735faa3b030cb5f4dc41c3c3bc31f0c
laramackey <lara.mackey@focusrite.com>
2021-02-18 16:16:22 +0000

    really beautiful game over modal

--- game.js
+++ game.js
@@ -144,6 +144,11 @@ class Game {
     }
   };
 
+  gameOver = () => {
+    const modal = document.getElementById('gameOverModal');
+    modal.style.display = 'block';
+  };
+
   findMidPointTile = (position) => {
     const midPointX = position.x * TILE_SIZE + TILE_SIZE / 2;
     const midPointY = position.y * TILE_SIZE + TILE_SIZE / 2;
@@ -241,6 +246,7 @@ class Game {
       this.drawCharacters();
       this.drawScore();
     } else {
+      this.gameOver();
       console.log('game over buddy');
     }
   };
